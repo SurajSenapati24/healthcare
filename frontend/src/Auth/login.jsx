6384bc9d48ec2a324d911935c83a1e9d8d5b9f37
Suraj Senapati <surajsenapati58@gmail.com>
2025-02-23 21:54:40 +0530

    Login signup

--- frontend/src/Auth/login.jsx
+++ frontend/src/Auth/login.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import "tailwindcss";
 const Login = () => {
   return (
-    <div class="bg-black w-300 j-items-center">
-      <form class="w-50 h-200">
-
-      </form>
-    </div>
+    <>
+      
+    </>
   )
 }
 
